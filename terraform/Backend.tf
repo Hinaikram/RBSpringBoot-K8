a62ce2aacc58d52b63bbd51cdd3d39e0f69f95d4
Hinaikram <hinaikramhh@gmail.com>
2025-04-14 09:55:26 +0000

    updated s3 configuration

--- terraform/Backend.tf
+++ terraform/Backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "rb-state-bucket"
     key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"   # optional but recommended
+    region         = "ap-northeast-3"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
