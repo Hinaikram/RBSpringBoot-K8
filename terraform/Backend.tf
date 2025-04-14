db536ef20eb1d9af2b8e9145289026815ecd0cf9
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-14 15:11:36 +0500

    Update Backend.tf

--- terraform/Backend.tf
+++ terraform/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rb-state-bucket"
+    bucket         = "rb-state-bucke1t"
     key            = "eks/terraform.tfstate"
     region         = "ap-northeast-3"
     dynamodb_table = "terraform-locks"
